e452a2ffc682614e30d7b3de9c7db12029f7019f
analog-alex <mumbleinout@gmail.com>
2024-06-30 15:02:12 +0100

    first draft of the readme:

--- main.go
+++ main.go
@@ -3,20 +3,21 @@ package main
 import (
 	"fmt"
 	"log"
-	"muzz-service/apis"
+	"muzz-service/api"
 	"muzz-service/config"
 )
 
 func main() {
-	log.Println("Booting up server in port 8080...")
+	log.Println("Starting...")
 
 	// get configs
 	port := config.GetApplicationConfig().Port
 
 	// get routes
-	router := apis.Routes()
+	router := api.Routes()
 
 	// start server
+	log.Println("Booting up server on port", port)
 	if err := router.Run(fmt.Sprintf(":%s", port)); err != nil {
 		// no server running, crash the program with no survivors
 		panic(err)
